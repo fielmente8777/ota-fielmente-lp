367cbf82c2937ec5769027d452357e499393cb12
sumit <sk7627577@gmail.com>
2025-07-31 14:04:14 +0530

    time update

--- src/components/PopupForm.tsx
+++ src/components/PopupForm.tsx
@@ -24,7 +24,7 @@ const PopupForm = ({
         setShowModal(true);
         setHasShown(true);
         document.body.style.overflow = "hidden";
-      }, 30000); // Show after 30 seconds
+      }, 40000); // Show after 30 seconds
     }
 
     return () => {
